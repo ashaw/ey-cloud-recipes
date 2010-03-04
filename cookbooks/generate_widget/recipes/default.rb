29b48a90ed25166b0dd0a88fef97582fa69452b9
Al Shaw <al@tpm-imac>
2010-03-04 09:17:30 -0500

    proper chef recipe for generate_widget and fix delayed_job template syntax

--- cookbooks/generate_widget/recipes/default.rb
+++ cookbooks/generate_widget/recipes/default.rb
@@ -3,4 +3,19 @@
 # Recipe:: default
 #
 
-`cd /data/polltracker/current && rake polltracker:widget RAILS_ENV=production`
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+
+	env = node[:environment][:framework_env]
+
+  # be sure to replace "app_name" with the name of your application.
+  run_for_app("polltracker") do |app_name, data| 
+    
+    bash "generate_widget" do
+       user "root"
+       code "cd /data/#{app_name}/current && rake polltracker:widget RAILS_ENV=#{env}"
+    end
+      
+  end
+  
+
+end
