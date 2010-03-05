10a97f706590e033ced55a8f1d48baba1da2fbfc
Al Shaw <al@tpm-imac>
2010-03-05 10:14:27 -0500

    run generate_widget as deploy

--- cookbooks/generate_widget/recipes/default.rb
+++ cookbooks/generate_widget/recipes/default.rb
@@ -11,7 +11,7 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   run_for_app("polltracker") do |app_name, data| 
     
     bash "generate_widget" do
-       user "root"
+       user "deploy"
        code "cd /data/#{app_name}/current && rake polltracker:widget RAILS_ENV=#{env}"
     end
       
